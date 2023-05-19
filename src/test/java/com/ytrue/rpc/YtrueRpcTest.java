194bd00957e0e3cc0de92fcac696cba7f573ac1c
ytrue <1151832035@qq.com>
2023-05-19 14:11:21 +0800

    规划目录

---
+++ src/test/java/com/ytrue/rpc/YtrueRpcTest.java
@@ -0,0 +1,16 @@
+package com.ytrue.rpc;
+
+import org.junit.Test;
+
+/**
+ * @author ytrue
+ * @date 2023-05-19 12:56
+ * @description YtrueRpcTest
+ */
+public class YtrueRpcTest {
+
+    @Test
+    public void test01() {
+        System.out.println(123);
+    }
+}
